d6a1dcdf608b3f666ecc9fa0a724cadc306e6397
pdmxdd <pdmxdd@gmail.com>
2021-07-28 12:42:16 -0500

    took out @RequestParam List<Skill> skills from processAddJobForm() they are explcitly asked to add this back in in part 3 I think.
    it breaks the early parts if they have this piece in the method signature. shouldn't be an issue now, if it is we will work through it as a class.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                    Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+                                    Errors errors, Model model, @RequestParam int employerId) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
